4f9911cabef1f78733ae8483fa9af7e6894233cc
Tai <taiwoodley@gmail.com>
2024-10-18 22:07:00 +1100

    Integrate UI with LNN

--- Models/LnnOutput.cs
+++ Models/LnnOutput.cs
@@ -5,7 +5,16 @@ namespace lnn_ui.Models;
 public class LnnOutput
 {
     [JsonPropertyName("index")] public int Index { get; init; }
-    [JsonPropertyName("prediction")] public double Prediction { get; init; }
-    [JsonPropertyName("temperature")] public int Temperature { get; init; }
-    [JsonPropertyName("vorticity")] public int Vorticity { get; init; }
+    [JsonPropertyName("nature")] public double Nature { get; init; }
+    [JsonPropertyName("intensity")] public double Intensity { get; init; }
+    [JsonPropertyName("month")] public double Month { get; init; }
+    [JsonPropertyName("day")] public double Day { get; init; }
+    [JsonPropertyName("hour")] public double Hour { get; init; }
+    [JsonPropertyName("lat")] public double Latitude { get; init; }
+    [JsonPropertyName("lon")] public double Longitude { get; init; }
+    [JsonPropertyName("wind")] public double Wind { get; init; }
+    [JsonPropertyName("pres")] public double Pressure { get; init; }
+    [JsonPropertyName("gust")] public double Gust { get; init; }
+    [JsonPropertyName("eye")] public double Eye { get; init; }
+    [JsonPropertyName("storm_speed")] public double StormSpeed { get; init; }
 }
--- Services/DataInputService.cs
+++ Services/DataInputService.cs
@@ -1,13 +1,23 @@
+using System.Globalization;
+
 namespace lnn_ui.Services;
 
 public class DataInputService(HttpClient client)
 {
-    public async Task<string> SendInput(int temperature, int vorticity)
+    public async Task<string> SendInput(float month, float day, float hour, float lat, float lon, float wind, float pres, float gust, float eye, float speed)
     {
         var values = new Dictionary<string, string>
         {
-            { "temperature", temperature.ToString() },
-            { "vorticity", vorticity.ToString() }
+            { "month", month.ToString(CultureInfo.CurrentCulture) },
+            { "day", day.ToString(CultureInfo.CurrentCulture) },
+            { "hour", hour.ToString(CultureInfo.CurrentCulture) },
+            { "lat", lat.ToString(CultureInfo.CurrentCulture) },
+            { "lon", lon.ToString(CultureInfo.CurrentCulture) },
+            { "wind", wind.ToString(CultureInfo.CurrentCulture) },
+            { "pres", pres.ToString(CultureInfo.CurrentCulture) },
+            { "gust", gust.ToString(CultureInfo.CurrentCulture) },
+            { "eye", eye.ToString(CultureInfo.CurrentCulture) },
+            { "speed", speed.ToString(CultureInfo.CurrentCulture) }
         };
         var content = new FormUrlEncodedContent(values);
         var response = await client.PostAsync("http://127.0.0.1:5000/set", content);
