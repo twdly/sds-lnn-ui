f6fab8fd71881326ae8b7150a8dda83b2bdf06f9
Tai <taiwoodley@gmail.com>
2024-10-23 13:41:16 +1100

    Improve error handling when API is not able to provide data

--- Services/PredictionService.cs
+++ Services/PredictionService.cs
@@ -10,6 +10,13 @@ public class PredictionService(HttpClient client, IConfiguration config)
         var apiUrl = config.GetConnectionString("ApiUrl");
         var response = client.GetAsync($"{apiUrl}/get");
         var result = await response;
-        return JsonSerializer.Deserialize<LnnOutput>(await result.Content.ReadAsStringAsync()) ?? new LnnOutput();
+        try
+        {
+            return JsonSerializer.Deserialize<LnnOutput>(await result.Content.ReadAsStringAsync()) ?? new LnnOutput();
+        }
+        catch (Exception)
+        {
+            return new LnnOutput();
+        }
     }
 }
